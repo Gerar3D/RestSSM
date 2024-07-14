cd6954ffe91a36ae4e19bb0d0ee7859c0ec7bf0f
Gerardo Peralta Cebrián <gerardo_cebrian@hotmail.com>
2024-07-13 22:55:02 -0600

    Menu bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { ArticuloComponent } from './pages/articulo/articulo.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { RubroComponent } from './pages/rubro/rubro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TabsComponent } from './core/components/tabs/tabs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "articulo",
+        component: ArticuloComponent
+    },
+    {
+        path: "buscar",
+        component: BuscarComponent
+    },
+    {
+        path: "carrito",
+        component: CarritoComponent
+    },
+    {
+        path: "perfil",
+        component: PerfilComponent
+    },
+    {
+        path: "rubro",
+        component: RubroComponent
+    }
+];
